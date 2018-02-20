7a66d49fa3c3c58a8cf521cbc4fd395afbdd9bb6
yurkevich-serj <yurkevichserj@gmail.com>
2018-02-20 20:05:09 +0300

    Finish second task

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+// module.exports = function getLoveTrianglesCount(preferences) {
+//   return 8;
+// };
+module.exports = function getLoveTrianglesCount(arr){
+	console.log(arr);
+	var count = 0;
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] <= arr.length) {
+			if (arr[arr[i] - 1] <= arr.length) {
+				if (arr[arr[arr[i] - 1] -1 ] === i+1) {
+					
+					count++;
+				} 
+			}
+		}
+	}
+	for (var i = 0; i < arr.length; i++) {
+		if (i+1 === arr[i]) {
+			count--;
+		}
+	}
+	return count/3;
+}
+
